fde48db291921f83c2ff976f568b3dac986fc859
Amirul Faqih <fakiiahmad001@gmail.com>
2024-09-03 18:15:12 +0300

    chore: Update test to check for "User Posts Overview" title

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders User Posts Overview title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/User Posts Overview/i);
+  expect(titleElement).toBeInTheDocument();
 });
+
